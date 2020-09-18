e1260e0229d973b45651800d69761092da6c125b
ravinaNG <ravina18@navgurukul.org>
2020-09-19 03:21:56 +0530

    assignment_of_js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var list = ['Rinki', 'Japan', 'jinger'];
+for (i = 0; i<list.length; i++){
+    // console.log(list[i]);
+    if (list[i][0]=='j'|| list[i][0]=='J'){
+        console.log("Goodbye", list[i]);
+        }
+    else{
+        console.log("Hello", list[i]);
+    }
+}
